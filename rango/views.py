6c27b75952a7dcd431c892854084157fb7eff506
George Vlachos <george@intermobilis.com>
2014-06-02 11:54:26 +0300

    get caterory list is now sorted by name

--- rango/views.py
+++ rango/views.py
@@ -21,9 +21,9 @@ def decode_url(url):
 def get_category_list(max_results=0, starts_with=''):
     cat_list = []
     if starts_with:
-        cat_list = Category.objects.filter(name__istartswith=starts_with)
+        cat_list = Category.objects.filter(name__istartswith=starts_with).order_by('name')
     else:
-        cat_list = Category.objects.all()
+        cat_list = Category.objects.all().order_by('name')
 
     if max_results > 0:
         if len(cat_list) > max_results:
