e9066f672616be3173aa819ab81aeb7d2d2829d5
George Vlachos <george@intermobilis.com>
2014-06-06 16:36:46 +0300

    show active navbar item

---
+++ rango/templatetags/tags.py
@@ -0,0 +1,8 @@
+from django import template
+register = template.Library()
+
+@register.simple_tag
+def active(request, pattern):
+    if request.path == pattern:
+        return 'active'
+    return ''
