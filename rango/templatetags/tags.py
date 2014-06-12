6e9c0a1e78e375c0e05e2202d203f09753dac2d5
George Vlachos <george@intermobilis.com>
2014-06-12 19:42:42 +0300

    register UI cosmetic redesign

--- rango/templatetags/tags.py
+++ rango/templatetags/tags.py
@@ -6,3 +6,19 @@ def active(request, pattern):
     if request.path == pattern:
         return 'active'
     return ''
+
+@register.filter
+def field_type(field):
+    type = field.field.widget.__class__.__name__
+    if type is 'TextInput':
+        return 'text'
+    elif type is 'PasswordInput':
+        return 'password'
+    elif type is 'EmailInput':
+        return 'email'
+    elif type is 'URLInput':
+        return 'url'
+    elif type is 'ClearableFileInput':
+        return 'file'
+    else:
+        return type
